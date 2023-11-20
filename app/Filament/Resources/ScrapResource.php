075546d855be77582f19727a05095a90b1cf1ff0
Yasin Korkmaz <yasinkorkmaz.dev@gmail.com>
2023-11-20 18:42:43 +0300

    Bulk Excel Export

--- app/Filament/Resources/ScrapResource.php
+++ app/Filament/Resources/ScrapResource.php
@@ -12,6 +12,7 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use pxlrbt\FilamentExcel\Actions\Tables\ExportBulkAction;
 
 class ScrapResource extends Resource
 {
@@ -64,7 +65,8 @@ class ScrapResource extends Resource
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
+                    //Tables\Actions\DeleteBulkAction::make(),
+                    ExportBulkAction::make()
                 ]),
             ]);
     }
