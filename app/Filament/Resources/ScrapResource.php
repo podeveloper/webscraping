d1cd900afa03846aab1e363d6305e4872c0835b8
Yasin Korkmaz <yasinkorkmaz.dev@gmail.com>
2023-11-21 10:55:18 +0300

    Refactoring ScrapResource

--- app/Filament/Resources/ScrapResource.php
+++ app/Filament/Resources/ScrapResource.php
@@ -24,13 +24,13 @@ class ScrapResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('source')
-                    ->maxLength(255),
                 Forms\Components\TextInput::make('category')
                     ->maxLength(255),
                 Forms\Components\TextInput::make('name')
                     ->maxLength(255),
                 Forms\Components\TextInput::make('url')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('source')
                     ->maxLength(255),
             ]);
     }
@@ -39,13 +39,13 @@ class ScrapResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('source')
-                    ->searchable(),
                 Tables\Columns\TextColumn::make('category')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('name')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('url')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('source')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
@@ -56,9 +56,42 @@ class ScrapResource extends Resource
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
             ])
+            ->paginated([10, 25, 50, 100])
             ->filters([
-                //
-            ])
+                Tables\Filters\Filter::make('category')
+                    ->form([
+                        Forms\Components\Select::make('category')
+                            ->options(Scrap::distinct()->pluck('category','category')->toArray()),
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        $category = $data['category'];
+                        return $category ? $query->where('category', '=', $category) : $query;
+                    }),
+                Tables\Filters\Filter::make('name')
+                    ->form([
+                        Forms\Components\TextInput::make('name'),
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        $name = $data['name'];
+                        return $name ? $query->where('name', 'like', '%' . $name . '%') : $query;
+                    }),
+                Tables\Filters\Filter::make('url')
+                    ->form([
+                        Forms\Components\TextInput::make('url'),
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        $url = $data['url'];
+                        return $url ? $query->where('url', 'like', '%' . $url . '%') : $query;
+                    }),
+                Tables\Filters\Filter::make('source')
+                    ->form([
+                        Forms\Components\TextInput::make('source'),
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        $source = $data['source'];
+                        return $source ? $query->where('source', 'like', '%' . $source . '%') : $query;
+                    }),
+            ], layout: Tables\Enums\FiltersLayout::AboveContentCollapsible)
             ->actions([
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
@@ -76,5 +109,10 @@ class ScrapResource extends Resource
         return [
             'index' => Pages\ManageScraps::route('/'),
         ];
+    }
+
+    public static function getNavigationBadge(): ?string
+    {
+        return Scrap::count();
     }
 }
